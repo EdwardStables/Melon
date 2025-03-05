2993f8a0ffd01b2a72c03bd275b07b89486702f1
Edward Stables <edward.stables1198@gmail.com>
2025-03-05 05:18:28 +0000

    Add topological DAG ordering function

--- src/simulate.zig
+++ src/simulate.zig
@@ -21,27 +21,41 @@ const DAG = struct {
     }
 };
 
-pub fn build_DAG(mod: module.Module, alloc: std.mem.Allocator) !DAG {
-    // Kahn's Algorithm (https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm)
-    // - Create set S of nodes with no incoming edges (aka, when incoming edges only belong to the source outputs)
-    // - Create empty list L of topologically sorted nodes
-    // Repeat until S is empty:
-    //     - Remove node s from S
-    //     - Add s to L
-    //     - For each node m with an edge e from n to m:
-    //          - Remove e from graph
-    //          - If m has no other incoming edges then insert m into L
+test "make graph" {
+    var inp = try ArrayList(module.Comb).initCapacity(std.testing.allocator, 2);
+    defer inp.deinit();
 
-    //Create the graph structure
+    try inp.append(try module.Comb.init(0, std.testing.allocator));
+    try inp.append(try module.Comb.init(1, std.testing.allocator));
+    defer inp.items[0].deinit();
+    defer inp.items[1].deinit();
+
+    try inp.items[0].inputs.append(0);
+    try inp.items[0].outputs.append(1);
+    try inp.items[0].outputs.append(2);
+
+    try inp.items[1].inputs.append(1);
+    try inp.items[1].outputs.append(3);
+
+    var g = try make_graph(inp, std.testing.allocator);
+    
+    try std.testing.expectEqual(2, g.items.len);
+    try std.testing.expectEqual(2, g.items[0].items.len);
+    try std.testing.expectEqual(2, g.items[1].items.len);
+    try std.testing.expect(!g.items[0].items[0]);
+    try std.testing.expect( g.items[0].items[1]);
+    try std.testing.expect(!g.items[1].items[0]);
+    try std.testing.expect(!g.items[1].items[1]);
+
+    g.items[0].deinit();
+    g.items[1].deinit();
+    g.deinit();
+}
+const Graph = ArrayList(ArrayList(bool));
+fn make_graph(comb: ArrayList(module.Comb), alloc: std.mem.Allocator) !Graph {
     //TODO: Really this should be its own struct with fully contiguous memory. Nested ArrayList has extra indirection
-    const table_size = mod.comb.items.len;
+    const table_size = comb.items.len;
     var graph = try ArrayList(ArrayList(bool)).initCapacity(alloc, table_size);
-    defer {
-        for (graph.items) |row| {
-            row.deinit();
-        }
-        graph.deinit();
-    }
 
     // Set all entries to false
     for (0..table_size) |_| {
@@ -53,8 +67,8 @@ pub fn build_DAG(mod: module.Module, alloc: std.mem.Allocator) !DAG {
     }
 
     // Set fields to true when there is an edge
-    for (mod.comb.items, 0..) |mod1, i| {
-        for (mod.comb.items, 0..) |mod2, j| {
+    for (comb.items, 0..) |mod1, i| {
+        for (comb.items, 0..) |mod2, j| {
             if (i==j) continue;
             
             output_loop: for (mod1.outputs.items) |src| {
@@ -68,23 +82,74 @@ pub fn build_DAG(mod: module.Module, alloc: std.mem.Allocator) !DAG {
         }
     }
 
-    // Search throw graph to build S (nodes where there are no inputs in the graph)
-    var S = ArrayList(module.CombID).init(alloc);
-    defer S.deinit();
+    return graph;
+}
 
-    for (0..table_size) |row| {
-        var has_input = false;
-        for (0..table_size) |col| {
+test "make S" {
+    var g = ArrayList(ArrayList(bool)).init(std.testing.allocator);
+    defer g.deinit();
+
+    // G is: ABC
+    //     A 000
+    //     B 001
+    //     C 000
+    //Meaning one edge from B to C
+    //Therefore S should be A,B as neither have any incoming edges
+
+    try g.append(ArrayList(bool).init(std.testing.allocator));
+    try g.items[0].append(false); try g.items[0].append(false); try g.items[0].append(false);
+    try g.append(ArrayList(bool).init(std.testing.allocator));
+    try g.items[1].append(false); try g.items[1].append(false); try g.items[1].append(true);
+    try g.append(ArrayList(bool).init(std.testing.allocator));
+    try g.items[2].append(false); try g.items[2].append(false); try g.items[2].append(false);
+    defer g.items[0].deinit(); defer g.items[1].deinit(); defer g.items[2].deinit();
+
+    var S = try make_S_array(g, std.testing.allocator);
+    defer S.deinit();
+    try std.testing.expectEqualSlices(module.CombID, &[_]module.CombID{0,1}, S.items);
+}
+
+fn make_S_array(graph: Graph, alloc: std.mem.Allocator) !ArrayList(module.CombID) {
+    var S =  ArrayList(module.CombID).init(alloc);
+    const table_size = graph.items.len;
+    for (0..table_size) |col| {
+        for (0..table_size) |row| {
             if (graph.items[row].items[col]) {
-                has_input = true;
                 break;
             }
-        }
-
-        if (!has_input) {
-            try S.append(@intCast(row));
+        } else {
+            try S.append(@intCast(col));
         }
     }
+
+    return S;
+}
+
+pub fn build_DAG(mod: module.Module, alloc: std.mem.Allocator) !DAG {
+    // Kahn's Algorithm (https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm)
+    // - Create set S of nodes with no incoming edges (aka, when incoming edges only belong to the source outputs)
+    // - Create empty list L of topologically sorted nodes
+    // Repeat until S is empty:
+    //     - Remove node s from S
+    //     - Add s to L
+    //     - For each node m with an edge e from n to m:
+    //          - Remove e from graph
+    //          - If m has no other incoming edges then insert m into L
+
+    //Create the graph structure
+    var graph = try make_graph(mod.comb, alloc);
+    defer {
+        for (graph.items) |row| {
+            row.deinit();
+        }
+        graph.deinit();
+    }
+
+    const table_size = mod.comb.items.len;
+
+    // Search throw graph to build S (nodes where there are no inputs in the graph)
+    var S = try make_S_array(graph, alloc);
+    defer S.deinit();
 
     // Create L ahead of time
     // Used in return, do not deinit
@@ -103,6 +168,9 @@ pub fn build_DAG(mod: module.Module, alloc: std.mem.Allocator) !DAG {
             }
         }
     }
+
+    assert(S.items.len == 0);
+    assert(L.items.len == mod.comb.items.len);
 
     return .{.comb_order=L};
 }
@@ -133,6 +201,7 @@ pub fn run_cycle(mod: module.Module, dag: DAG) SimulationError!?SimulationResult
 }
 
 test "one item DAG" {
+    // --s1-->| comb |--s2-->
     var mod = try module.Module.init(0, std.testing.allocator);
     defer mod.deinit();
 
@@ -146,5 +215,73 @@ test "one item DAG" {
     var dag = try build_DAG(mod, std.testing.allocator);
     defer dag.deinit();
 
-    assert(std.mem.eql(module.CombID, dag.comb_order.items, &[_]module.CombID{0}));
+    try std.testing.expectEqualSlices(module.CombID, &[_]module.CombID{comb}, dag.comb_order.items);
+}
+
+test "two item DAG" {
+    // --s1-->| comb1 |--s2-->| comb2 |--s3-->
+    var mod = try module.Module.init(0, std.testing.allocator);
+    defer mod.deinit();
+
+    const s1 = try mod.addSignal(1);
+    const s2 = try mod.addSignal(1);
+    const s3 = try mod.addSignal(1);
+    const comb1 = try mod.addComb();
+    const comb2 = try mod.addComb();
+
+    try mod.addCombInput(comb1, s1);
+    try mod.addCombOutput(comb1, s2);
+    try mod.addCombInput(comb2, s2);
+    try mod.addCombOutput(comb2, s3);
+
+    var dag = try build_DAG(mod, std.testing.allocator);
+    defer dag.deinit();
+
+    try std.testing.expectEqualSlices(module.CombID, &[_]module.CombID{comb1, comb2}, dag.comb_order.items);
+}
+
+test "complex DAG" {
+    //
+    // --s1-->| comb4 |--s3-->| comb2 |--s5-->
+    //  |---->|            |->|
+    //  |  |->|            |------<------
+    //  |  |------------|               |
+    // --s2-->| comb3 |--s4-->| comb1 |--s6-->
+    //                  -s1-->|
+    var mod = try module.Module.init(0, std.testing.allocator);
+    defer mod.deinit();
+
+    const s1 = try mod.addSignal(1);
+    const s2 = try mod.addSignal(1);
+    const s3 = try mod.addSignal(1);
+    const s4 = try mod.addSignal(1);
+    const s5 = try mod.addSignal(1);
+    const s6 = try mod.addSignal(1);
+
+    const comb1 = try mod.addComb();
+    const comb2 = try mod.addComb();
+    const comb3 = try mod.addComb();
+    const comb4 = try mod.addComb();
+
+    //Comb4
+    try mod.addCombInput(comb4, s1);
+    try mod.addCombInput(comb4, s2);
+    try mod.addCombInput(comb4, s4);
+    try mod.addCombOutput(comb4, s3);
+    //Comb3
+    try mod.addCombInput(comb3, s2);
+    try mod.addCombOutput(comb3, s4);
+    //Comb2
+    try mod.addCombInput(comb2, s3);
+    try mod.addCombInput(comb2, s6);
+    try mod.addCombOutput(comb2, s5);
+    //Comb1
+    try mod.addCombInput(comb1, s4);
+    try mod.addCombInput(comb1, s1);
+    try mod.addCombOutput(comb1, s6);
+
+    var dag = try build_DAG(mod, std.testing.allocator);
+    defer dag.deinit();
+
+    try std.testing.expectEqualSlices(module.CombID, &[_]module.CombID{comb3, comb4, comb1, comb2}, dag.comb_order.items);
 }
